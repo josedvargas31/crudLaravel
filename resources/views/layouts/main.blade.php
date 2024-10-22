ad2ac762c49b636c9927db17211c20a8fa29d08e
josedvargas31 <dajozavargas@gmail.com>
2024-10-21 22:25:23 -0500

    Implementando el header con tailwins

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,13 +8,20 @@
     @vite('resources/css/app.css')
     <title>Mi primer crud con Laravel 11</title>
 </head>
+
 <body class="font-sans bg-gray-100">
-     <!-- Lo que hace es llamar cierta parte de un codigo cuando estemos en esta plantilla -->
-      {{-- Aca se declara --}}
-      @yield('contenido')
+  
+    <header class="p-5 border-b bg-white shadow flex justify-between items-center">
+        <img class="rounded-xl w-12" src="{{ asset('img/logoD.svg') }}" alt="Imagen de registro">
+        <h1 class="text-3xl font-black">
+            Crud
+        </h1>
+    </header>
 
+    <!-- Lo que hace es llamar cierta parte de un codigo cuando estemos en esta plantilla -->
+    {{-- Aca se declara --}}
+    @yield('contenido')
 
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"
-		></script>
-  </body>
+</body>
+
 </html>
--- resources/views/modules/users/index.blade.php
+++ resources/views/modules/users/index.blade.php
@@ -5,10 +5,11 @@
 @section('contenido')
 <div class="mt-4">
     <h2 class="text-center text-2xl font-bold">Crud con laravel 11 - Nombres de los usuarios</h2>
+    {{-- <img class="rounded-xl w-12" src="{{ asset('img/logoD.svg') }}" alt="Logo"> --}}
     <div class="flex justify-center p-4">
         <div class="inline-block bg-black shadow-md rounded-lg"> {{-- inline block - ayuda a que el contendedor blanco se ajuste al texto --}}
           <div class="p-4">
-            <a href="{{ route('create') }}" class="text-white text-center text-2xl font-bold">Agregar</a>
+            <a href="{{ route('create') }}" class="text-white text-center text-2xl font-bold hover">Agregar</a>
           </div>
         </div>
       </div>
