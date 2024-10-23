88c24b0f5c1f702a1faf32f5fc87315853fea8f3
josedvargas31 <dajozavargas@gmail.com>
2024-10-22 20:51:46 -0500

    Implementando la tabla de los usuarios

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,11 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Mi primer crud con Laravel 11</title>
 </head>
 
 <body class="font-sans bg-gray-100">
-  
+
     <header class="p-5 border-b bg-white shadow flex justify-between items-center">
         <img class="rounded-xl w-12" src="{{ asset('img/logoD.svg') }}" alt="Imagen de registro">
         <h1 class="text-3xl font-black">
@@ -23,5 +24,4 @@
     @yield('contenido')
 
 </body>
-
 </html>
--- resources/views/modules/users/index.blade.php
+++ resources/views/modules/users/index.blade.php
@@ -3,15 +3,56 @@
 {{-- Declaramos en donde va a trabajar, en este caso, trabajara en el archivo main --}}
 {{-- Aqui se utiliza --}}
 @section('contenido')
-<div class="mt-4">
-    <h2 class="text-center text-2xl font-bold">Crud con laravel 11 - Nombres de los usuarios</h2>
-    {{-- <img class="rounded-xl w-12" src="{{ asset('img/logoD.svg') }}" alt="Logo"> --}}
-    <div class="flex justify-center p-4">
-        <div class="inline-block bg-black shadow-md rounded-lg"> {{-- inline block - ayuda a que el contendedor blanco se ajuste al texto --}}
-          <div class="p-4">
-            <a href="{{ route('create') }}" class="text-white text-center text-2xl font-bold hover">Agregar</a>
-          </div>
+    <div class="mt-4">
+        <h2 class="text-center text-2xl font-bold">Crud con laravel 11 - Nombres de los usuarios</h2>
+        {{-- <img class="rounded-xl w-12" src="{{ asset('img/logoD.svg') }}" alt="Logo"> --}}
+        <div class="flex justify-center p-4">
+            <div class="inline-block bg-black shadow-md rounded-lg"> {{-- inline block - ayuda a que el contendedor blanco se ajuste al texto --}}
+                <div class="p-4">
+                    <a href="{{ route('create') }}" class="text-white text-center text-2xl font-bold hover">
+                        <i class="bi bi-plus"></i>Agregar nombre
+                    </a>
+                </div>
+            </div>
         </div>
-      </div>
-</div>
+
+
+        <div class="flex relative justify-center overflow-x-auto sm:rounded-lg">
+            <table class="container w-8/12 text-sm text-left rtl:text-right text-gray-500">
+                <thead class="text-xs uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            Id
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Nombre
+                        </th>
+                        {{-- <th scope="col" class="px-6 py-3">
+                            Category
+                        </th> --}}
+                        <th scope="col" class="px-6 py-3">
+                            Action
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr
+                        class="dark:bg-gray-900 border-b ">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            Apple MacBook Pro 17
+                        </th>
+                        <td class="px-6 py-4">
+                            Silver
+                        </td>
+
+                        <td class="px-6 py-4">
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Mostrar</a>
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                            <button></button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
